24aebb39f11f65a50a32b1b10e614f6b6fd0b390
[Scrummibears] Brad Johnson <bjohnson@vendasta.com>
2019-08-07 15:00:50 -0600

    Working on getting build statuses written to csv

--- gcloud/run.sh
+++ gcloud/run.sh
@@ -3,5 +3,29 @@ export CLOUDSDK_CORE_PROJECT=repcore-prod;
 
 BUILD_ID='7a5e47a6-5388-4314-aa83-0a7563ea9b6c';
 
-desc=$(gcloud builds describe $BUILD_ID --format="flattened(steps[].id,steps[].status)")
-printf $desc
+echo "buildId,status" > out.va
+
+function checkBuildStatus() {
+	buildId=$1
+	output=$(gcloud builds describe $buildId --format="flattened(steps[].id,steps[].status)")
+
+	isLintNext=false
+
+	while read x; do
+		if [[ $x == *ng-lint ]]; then
+			isLintNext=true;
+			continue;
+		fi
+		if [[ $isLintNext = true ]]; then
+			if [[ $x == *SUCCESS ]]; then
+				echo "$buildId,SUCCESS" >> out.va
+			elif [[ $x == *FAILURE ]];
+			then
+				"$buildId,SUCCESS" >> out.va
+			fi
+			break;
+		fi		
+	done <<< $output
+}
+
+checkBuildStatus $BUILD_ID;
