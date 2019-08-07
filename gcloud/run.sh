46245806f258b83019852b62fdfb83459cd23255
[Scrummibears] Brad Johnson <bjohnson@vendasta.com>
2019-08-07 17:36:02 -0600

    Added limit

--- gcloud/run.sh
+++ gcloud/run.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 export CLOUDSDK_CORE_PROJECT=repcore-prod;
 
+if [[ -z "$1" ]]; then
+	echo "No file provided"
+	echo "Usage:\n\trun.sh file-containing-build-ids"
+	exit -1;
+fi
+
+lines=$(cat $1 | wc -l)
+if [[ $lines -gt 3600 ]]; then
+	echo "$lines is too many requests to make.  It will take more than 1 hour.";
+	exit -1;
+fi
+
 BUILD_ID='7a5e47a6-5388-4314-aa83-0a7563ea9b6c';
 
 echo "buildId,status" > out.va
 
 function checkBuildStatus() {
 	buildId=$1
+
+
+	echo "Checking status of build $buildId"
 	output=$(gcloud builds describe $buildId --format="flattened(steps[].id,steps[].status)")
 
 	isLintNext=false
@@ -21,14 +36,22 @@ function checkBuildStatus() {
 				echo "$buildId,SUCCESS" >> out.va
 			elif [[ $x == *FAILURE ]];
 			then
-				"$buildId,SUCCESS" >> out.va
+				echo "$buildId,FAILURE" >> out.va
 			fi
-			break;
+			return;
 		fi		
 	done << EOF
 	$output
 EOF
 }
 
-checkBuildStatus $BUILD_ID;
+while IFS= read -r line
+do
+	if [[ $line == build_id ]]; then
+		continue;
+	fi
+  	checkBuildStatus $line
+done < "$1"
+
+
 
