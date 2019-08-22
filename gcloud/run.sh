fee697f6b02204171d8ff17351ed9c8739c4a01b
[Scrummibears] Brad Johnson <bjohnson@vendasta.com>
2019-08-22 11:27:42 -0600

    Automate fetching of builds

--- gcloud/run.sh
+++ gcloud/run.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 export CLOUDSDK_CORE_PROJECT=repcore-prod;
 
-if [[ -z "$1" ]]; then
-	echo "No file provided"
-	echo "Usage:\n\trun.sh file-containing-build-ids"
-	exit -1;
-fi
+#if [[ -z "$1" ]]; then
+#	echo "No file provided"
+#	echo "Usage:\n\trun.sh file-containing-build-ids"
+#	exit -1;
+#fi
 
-lines=$(cat $1 | wc -l)
-if [[ $lines -gt 3600 ]]; then
-	echo "$lines is too many requests to make.  It will take more than 1 hour.";
-	exit -1;
-fi
+buildIds=$(gcloud builds list --format="get(ID)" --page-size=20)
 
 BUILD_ID='7a5e47a6-5388-4314-aa83-0a7563ea9b6c';
 
@@ -34,24 +30,26 @@ function checkBuildStatus() {
 		if [[ $isLintNext = true ]]; then
 			if [[ $x == *SUCCESS ]]; then
 				echo "$buildId,SUCCESS" >> out.va
+				return;
 			elif [[ $x == *FAILURE ]];
 			then
 				echo "$buildId,FAILURE" >> out.va
+				return
 			fi
 			return;
 		fi		
 	done << EOF
 	$output
 EOF
+echo "$buildId,???" >> out.va
 }
 
-while IFS= read -r line
+while IFS= read line;
 do
 	if [[ $line == build_id ]]; then
 		continue;
 	fi
   	checkBuildStatus $line
-done < "$1"
-
-
-
+done << EOF
+	$buildIds
+EOF
